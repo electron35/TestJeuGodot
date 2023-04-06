ec2a0f78f5583295df8f7e928035f55069edf5e7
electron35 <eleque.turone@outlook.com>
2023-04-06 22:51:36 +0200

    Added Sprint

--- scene/Player/Player.gd
+++ scene/Player/Player.gd
@@ -11,14 +11,20 @@ func _process(delta):
 	pass
 
 func move(delta):
+	var sprint = 1
 	direction= 0
+	
 	if Input.is_action_pressed("MoveLeft"):
 		direction = -1
 	if Input.is_action_pressed("MoveRight"):
 		direction = 1
+	if Input.is_action_pressed("Sprint"):
+		sprint = 1.5
+	else:
+		sprint = 1
 		
 	var velocity = Vector2.ZERO
-	velocity.x = direction * speed
+	velocity.x = (direction * speed)*sprint
 	
 	position += velocity * delta
 	
